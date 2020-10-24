0d4a47a4df4900dc4a895bd77a16b8c051ffc6ef
Omar Monrroy <ogcm.kael@hotmail.com>
2020-10-24 06:02:49 -0500

    Eliminando componente de expo

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
@@ -10,7 +9,6 @@ export default function App() {
         <Button title="ADD" />
       </View>
       <View></View>
-      <StatusBar style="auto" />
     </View>
   );
 }
